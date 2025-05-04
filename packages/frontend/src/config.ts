7c12625ebe8f9cb8c766fda63b762264b3e058c2
Michael Schmidt <m.schmidt@resolution.de>
2025-05-04 15:45:12 +0200

    package change

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -1,7 +1,8 @@
 declare const __DEV__: boolean;
 
 export const BACKEND_URL = __DEV__
-    ? 'https://lafisrap.ngrok.io' // Development - using your local IP
+    ? // ? 'https://lafisrap.ngrok.io' // Development - using your local IP
+      'https://gedankenfehler-production.up.railway.app' // Development - using your local IP
     : 'https://gedankenfehler-production.up.railway.app'; // Production
 
 export const CARD_STYLES = {
