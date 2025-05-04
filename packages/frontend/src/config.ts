26d84b48346544fa573f55446de017c65efa4ca0
Michael Schmidt <m.schmidt@resolution.de>
2025-05-04 13:41:05 +0200

    Fix for android

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -2,7 +2,7 @@ declare const __DEV__: boolean;
 
 export const BACKEND_URL = __DEV__
     ? 'https://lafisrap.ngrok.io' // Development - using your local IP
-    : 'https://12weltanschauungenserver-production.up.railway.app'; // Production
+    : 'https://gedankenfehler-production.up.railway.app'; // Production
 
 export const CARD_STYLES = {
     default: 'defaultStyle',
