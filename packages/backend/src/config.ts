798625f42bcb4eb2c9d0dcfb54580b6aa0c6613f
Michael Schmidt <m.schmidt@resolution.de>
2025-05-07 12:53:00 +0200

    Removed packages/backend

--- packages/backend/src/config.ts
+++
@@ -1,5 +0,0 @@
-export const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-    throw new Error('MONGODB_URI is not set');
-}
--- packages/backend/src/routers/index.ts
+++
@@ -1,7 +0,0 @@
-import { initTRPC } from '@trpc/server';
-
-export const URL_STATIC = '/static';
-
-const t = initTRPC.create();
-export const publicProcedure = t.procedure;
-export const router = t.router;
