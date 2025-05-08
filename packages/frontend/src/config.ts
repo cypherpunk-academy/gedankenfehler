f950db3dd17f5d9542b667a7046aca859bc7e780
Michael Schmidt <m.schmidt@resolution.de>
2025-05-08 09:44:19 +0200

    Version bump 20.18.1

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -2,8 +2,7 @@ declare const __DEV__: boolean;
 
 export const BACKEND_URL = __DEV__
     ? 'https://lafisrap.ngrok.io' // Development - using your local IP
-    : //   'https://gedankenfehler-production.up.railway.app' // Development - using your local IP
-      'https://gedankenfehler-production.up.railway.app'; // Production
+    : 'https://weltanschauungen-backend-production.up.railway.app'; // Production
 
 export const CARD_STYLES = {
     default: 'defaultStyle',
